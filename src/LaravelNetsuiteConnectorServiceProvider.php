4296e3cfc1a27b9fde86fc00e84196628537dd7a
Florian Ratz <florian.ratz@supsign.ch>
2022-10-05 09:44:56 +0200

    naming

--- src/LaravelNetsuiteConnectorServiceProvider.php
+++ src/LaravelNetsuiteConnectorServiceProvider.php
@@ -4,7 +4,7 @@ namespace Supsign\NetsuiteConnector;
 
 use Illuminate\Support\ServiceProvider;
 
-class NetsuiteConnectorServiceProvider extends ServiceProvider
+class LaravelNetsuiteConnectorServiceProvider extends ServiceProvider
 {
 
     public function boot() 
