499fd9edaed51c19cb126fd87cf906e81ceb80e6
Florian Ratz <florian.ratz@supsign.ch>
2022-10-05 09:35:15 +0200

    init

---
+++ src/NetsuiteConnector.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Supsign\NetsuiteConnector;
+
+class NetsuiteConnector {
+    
+}
---
+++ src/NetsuiteConnectorServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Supsign\NetsuiteConnector;
+
+use Illuminate\Support\ServiceProvider;
+
+class NetsuiteConnectorServiceProvider extends ServiceProvider
+{
+
+    public function boot() 
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
