607f9b36e145251f19c382d9daa4720c2a29fa9a
Florian Ratz <florian.ratz@supsign.ch>
2022-10-05 11:56:26 +0200

    no message

--- src/database/migrations/2022_10_05_090907_netsuite_connector_init.php
+++ src/database/migrations/2022_10_05_090907_netsuite_connector_init.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('netsuite_calls', function (Blueprint $table) {
+        Schema::create('net_suite_calls', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_parsed')->default(0);
             $table->string('method')->nullable();
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('netsuite_calls');
+        Schema::dropIfExists('net_suite_calls');
     }
 };
