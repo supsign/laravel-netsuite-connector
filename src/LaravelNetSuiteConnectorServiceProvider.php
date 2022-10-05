35dfa017ade570ecea132de6659a3bcb73d476e2
Florian Ratz <florian.ratz@supsign.ch>
2022-10-05 12:56:44 +0200

    with file?

--- src/LaravelNetSuiteConnectorServiceProvider.php
+++ src/LaravelNetSuiteConnectorServiceProvider.php
@@ -10,7 +10,7 @@ class LaravelNetSuiteConnectorServiceProvider extends ServiceProvider
     public function boot() 
     {
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/routes');
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
     }
 
     public function register()
